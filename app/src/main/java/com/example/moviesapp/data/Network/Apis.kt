a9c4b935a1d6601fd1dd9dc34b9f4c7ca3b3388b
Marwa Mubarak <marwamubarak6@gmail.com>
2022-09-14 17:42:42 +0200

    Update Apis.kt

--- app/src/main/java/com/example/moviesapp/data/Network/Apis.kt
+++ app/src/main/java/com/example/moviesapp/data/Network/Apis.kt
@@ -1,21 +1,24 @@
 package com.example.moviesapp.data.Network
 
+//import com.example.moviesapp.data.Models.RegisterResponse
 import com.example.moviesapp.data.Models.LoginRequest
 import com.example.moviesapp.data.Models.LoginResponse
-import com.example.moviesapp.data.Models.RegisterRequest
-import com.example.moviesapp.data.Models.RegisterResponse
 import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 import retrofit2.http.POST
+<<<<<<< Updated upstream
+=======
+
+>>>>>>> Stashed changes
 
 //https://our-movie-service.herokuapp.com/Users/login
 //https://our-movie-service.herokuapp.com/Users/register
 
 interface Apis {
-    @POST("Users/register")
-    suspend fun register(@Body registerRequest: RegisterRequest): Response<RegisterResponse>
+//    @POST("Users/register")
+//    suspend fun register(@Body registerRequest: RegisterRequest): Response<RegisterResponse>
 
     @POST("Users/login")
     suspend fun login(@Body loginRequest: LoginRequest):Response<LoginResponse>
