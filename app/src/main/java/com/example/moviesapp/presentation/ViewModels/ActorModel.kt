0096b8c0074975a5d0eb794ea751de926b97bec4
Marwa Mubarak <marwamubarak6@gmail.com>
2022-09-14 17:42:01 +0200

    Create ActorModel.kt

---
+++ app/src/main/java/com/example/moviesapp/presentation/ViewModels/ActorModel.kt
@@ -0,0 +1,10 @@
+package com.example.moviesapp.presentation.ViewModels
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class ActorModel(
+    val image:Int,
+    val name:String,
+    val id:Int,
+    val moviesList:String):Parcelable
